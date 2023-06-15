46e73272192ad0059596be91222dd66019a5aa14
Ankit Navaghare <ankitnavaghare12345@gmail.com>
2023-06-15 19:03:30 +0530

    Second commit - send message

---
+++ lib/ChatMessage.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class ChatMessage extends StatelessWidget {
+  const ChatMessage({super.key, required this.text, required this.name});
+
+  final String text;
+  final String name;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 5.0),
+      child: Row(
+        children: [
+          Container(
+            margin: const EdgeInsets.only(right: 16.0),
+            child: const CircleAvatar(child: Text("B")),
+          ),
+          Expanded(
+            child: Text(text, style: TextStyle(color: Colors.white)),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/chatscreen.dart
+++ lib/chatscreen.dart
@@ -1,6 +1,10 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+
+import 'ChatMessage.dart';
 
 class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
@@ -10,6 +14,26 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  final List<ChatMessage> _messages = [];
+  final TextEditingController _controller = TextEditingController();
+
+  void _sendMessage() {
+    final String text = _controller.text;
+
+    if (_controller.text.isNotEmpty) {
+      ChatMessage message = ChatMessage(
+        text: text,
+        name: "User",
+      );
+
+      setState(() {
+        _messages.insert(0, message);
+      });
+
+      _controller.clear();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
@@ -28,39 +52,47 @@ class _ChatScreenState extends State<ChatScreen> {
           ),
         ),
       ),
-      body: Column(children: [
-        Expanded(
-          child: Container(height: size.height),
-        ),
-        Padding(
-          padding: EdgeInsets.only(
-              left: size.width * 0.025, right: size.width * 0.025, bottom: 15),
-          child: SizedBox(
-            // height: size.height * 0.78,
-            width: size.width * 0.95,
-            child: TextField(
-              style: TextStyle(color: Colors.white),
-              decoration: InputDecoration(
-                // contentPadding: EdgeInsets.only(right: 20),
-                suffixIcon: IconButton(
-                  onPressed: () {},
-                  icon: Icon(
-                    Icons.send,
-                    color: Color(0xFF797E8B),
-                  ),
-                ),
-                hintStyle: TextStyle(color: Color(0xFF797E8B)),
-                hintText: "Type a message...",
-                filled: true,
-                fillColor: Color(0xFF40414F),
-                border:
-                    OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+      body: SafeArea(
+        child: Column(children: [
+          Flexible(
+            // child: Container(height: size.height),
+            child: Padding(
+              padding: EdgeInsets.all(16.0),
+              child: ListView.builder(
+                reverse: true,
+                itemCount: _messages.length,
+                itemBuilder: (context, index) {
+                  return _messages[index];
+                },
               ),
             ),
           ),
-        ),
-      ]),
-      // ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(16.0, 0, 8.0, 16.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    showCursor: _controller.text.isNotEmpty ? true : false,
+                    enableInteractiveSelection: false,
+                    controller: _controller,
+                    decoration: InputDecoration(
+                        hintText: "Type a message...",
+                        hintStyle: TextStyle(color: Color(0xFF797E8B)),
+                        border: InputBorder.none),
+                  ),
+                ),
+                IconButton(
+                  icon: Icon(Icons.send),
+                  onPressed: () {
+                    _sendMessage();
+                  },
+                )
+              ],
+            ),
+          ),
+        ]),
+      ),
     );
   }
 }
